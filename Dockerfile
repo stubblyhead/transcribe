d8c305f2a3dd1634b781881f0149945ec91a4011
EC2 Default User <ec2-user@ip-172-31-19-96.us-west-2.compute.internal>
2024-03-16 04:02:56 +0000

    this doesn't really work like I want

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM public.ecr.aws/lambda/python:3.10
-RUN yum -y install git wget tar xz
+FROM public.ecr.aws/lambda/python:3.11
+RUN yum -y install git wget tar xz tree
 RUN pip install --no-cache-dir faster_whisper
 RUN pip install --no-cache-dir ffmpeg-python
-RUN wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz && tar xvf ffmpeg-release-amd64-static.tar.xz && mv ffmpeg-6.1-amd64-static/ff* /usr/bin && rm -Rf ffmpeg*
+RUN wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-arm64-static.tar.xz && tar xvf ffmpeg-release-arm64-static.tar.xz && mv ffmpeg-6.1-arm64-static/ff* /usr/bin && rm -Rf ffmpeg*
 COPY model.py /tmp
 RUN python /tmp/model.py && rm /tmp/model.py
 #RUN curl -O https://lambda-insights-extension.s3-ap-northeast-1.amazonaws.com/amazon_linux/lambda-insights-extension.rpm && \
--- transcribe.py
+++ transcribe.py
@@ -1,6 +1,6 @@
 from faster_whisper import WhisperModel
 import ffmpeg
-import multiprocessing
+from multiprocessing import cpu_count, Process, Pipe
 from typing import List
 import concurrent.futures
 import time
@@ -8,33 +8,56 @@ import os
 from split import split_audio_into_chunks
 
 
-def transcribe_file(file_path, model):
+def transcribe_file(file_path, model, conn):
+    print(f'transcribing {os.path.basename(file_path)} from parallel process')
     segments, info = model.transcribe(file_path)
+    print(f'finished transcribing {os.path.basename(file_path)}, a {info.duration} sec long audio file')
     segments = list(segments)
-    return segments
+    conn.send([segments])
+    conn.close
 
 
 def transcribe_audio(input_file: str, max_processes = 0,
                      silence_threshold: str = "-20dB", silence_duration: float = 2.0, model=None) -> str:
-    if max_processes > multiprocessing.cpu_count() or max_processes == 0:
-        max_processes = multiprocessing.cpu_count()
+    if max_processes > cpu_count() or max_processes == 0:
+        max_processes = cpu_count()
 
     # Split the audio into chunks
+    print('splitting audio into chunks')
     temp_files_array = split_audio_into_chunks(input_file, max_processes, silence_threshold, silence_duration)
     start = time.time()
     futures = []
-    # Submit each file to the thread pool and store the corresponding future object
-    with concurrent.futures.ThreadPoolExecutor(max_processes) as executor:
-        for file_path in temp_files_array:
-            future = executor.submit(transcribe_file, file_path, model)
-            futures.append(future)
+    processes = []
+    parent_connections = []
+#    print(f'starting {max_processes} concurrent transcription process')
+    for file_path in temp_files_array:
+        parent_conn, child_conn = Pipe()
+        parent_connections.append(parent_conn)
+        process = Process(target=transcribe_file,args=(file_path, model, child_conn))
+        processes.append(process)
+    for process in processes:
+        print(f'starting process for {os.path.basename(file_path)}')
+        process.start()
 
-    result_string = ""
-    for future in futures:
-        segments = future.result()
-        for seg in segments:
+    for process in processes:
+        process.join()
+
+    result_string = ''
+    for parent_connection in parent_connections:
+        result_string += parent_connection.recv()[0]
+
+    # Submit each file to the thread pool and store the corresponding future object
+#    with concurrent.futures.ThreadPoolExecutor(max_processes) as executor:
+#        for file_path in temp_files_array:
+#            future = executor.submit(transcribe_file, file_path, model)
+#            futures.append(future)
+
+#   result_string = ""
+#   for future in futures:
+#        segments = future.result()
+#        for seg in segments:
         #    result_string += "".join(segment.text for segment in segments)
-            result_string += f"{seg.text}\n"
+#            result_string += f"{seg.text}\n"
 
     end = time.time()
     print(end - start)
